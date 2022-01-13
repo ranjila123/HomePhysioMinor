4ac964092fad955cc46d8dd14c33432420ca2013
Your Name <urjabajj2013@gmail.com>
2022-01-13 23:11:01 +0545

    Updated models

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomePhysio.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string CatInfo { get; set; }
+    }
+}
