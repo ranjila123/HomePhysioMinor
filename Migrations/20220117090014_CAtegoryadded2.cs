b498714536b43703c6dbfef8fd94e0015867e785
Monika Shakya <monikashakya1234@gmail.com>
2022-01-17 14:57:29 +0545

    migrations for category

---
+++ Migrations/20220117090014_CAtegoryadded2.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomePhysio.Migrations
+{
+    public partial class CAtegoryadded2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into CategoryModel(Name,CatInfo) values('General Physiotherapist','Physiotherapist for all')");
+            migrationBuilder.Sql("insert into CategoryModel(Name,CatInfo) values('Pediactric Physiotherapist','Physiotherapist for children')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
