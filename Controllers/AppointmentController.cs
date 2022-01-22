9033021c472ac6b91058d7b7f15cbd11b4748a33
Your Name <urjabajj2013@gmail.com>
2022-01-22 19:41:47 +0545

    AddedAnjularJs

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using HomePhysio.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomePhysio.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public AppointmentController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+      
+        public IActionResult Index()
+        {
+            var physio = _applicationDbContext.PhysiotherapistModel.ToList();
+            
+            return View(physio);
+        }
+        [Authorize]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/AngularServices/Controller/AppointmentController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('startapp')
+        .controller('AppointmentController', ['$scope', function AppointmentController($scope) {
+          
+            $scope.products = "Test";
+            console.log($scope.asd);
+        }]);
+})();
