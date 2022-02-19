b42a1cf8733373e35ad16d9f26effbdf557512e4
Monika Shakya <monikashakya1234@gmail.com>
2022-02-19 13:50:38 +0545

    seeding for Pstatus model and payment type

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -121,6 +121,27 @@ namespace HomePhysio.Controllers
 
            
         }
+
+        public static double toRad(double Value)
+        {
+            return (Value * Math.PI / 180);
+        }
+
+        public double calcCrow(double latt1, double lon1, double latt2, double lon2)
+        {
+            var R = 6371; // km
+            var dLat = toRad(latt2 - latt1);
+            var dLon = toRad(lon2 - lon1);
+            var lat1 = toRad(latt1);
+            var lat2 = toRad(latt2);
+
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+              Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c;
+            return d;
+        }
+
         [HttpPost]
         public async Task<IActionResult> ConfirmAppointment(int appointmentId)
         {
@@ -135,6 +156,30 @@ namespace HomePhysio.Controllers
                 appointment.StatusCode = "1";
                 _applicationDbContext.Update(appointment);
                 await _applicationDbContext.SaveChangesAsync();
+                var patient = await _applicationDbContext.PatientModel.SingleOrDefaultAsync(x => x.PatientId == appointment.PatientId);
+                var physio = await _applicationDbContext.PhysiotherapistModel.SingleOrDefaultAsync(x => x.PhysiotherapistId == appointment.PhysioTimeSlotsData.PhysiotherapistId);
+
+                var distance = calcCrow(double.Parse(patient.Latitude), double.Parse(patient.Longitude), double.Parse(physio.Latitude), double.Parse(physio.Longitude));
+                var amount = 0;
+                if (distance < 5)
+                {
+                    amount = 50;
+                }
+                else
+                {
+                    amount = 100;
+                }
+                var paymentsModel = new PaymentsModel
+                {
+                    AppointmentId = appointmentId,
+                    PaymentTypeId = 1,
+                    Amount = (int)physio.ConsultationCharge,
+                    DistanceAmount = amount,
+                    PStatusCode = "1" //unpaid
+                };
+                _applicationDbContext.Add(paymentsModel);
+                await _applicationDbContext.SaveChangesAsync();
+
                 return Json(new { result = true, msg = "Success" });
 
              }
